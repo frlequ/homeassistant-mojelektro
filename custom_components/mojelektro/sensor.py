bc32130633ee1ffbeb3112e3ce78f5bf42aa4ed0
frlequ <121799877+frlequ@users.noreply.github.com>
2024-01-28 15:32:21 +0100

    Update sensor.py

--- custom_components/mojelektro/sensor.py
+++ custom_components/mojelektro/sensor.py
@@ -1,8 +1,15 @@
 """Platform for sensor integration."""
+from __future__ import annotations
+
+from homeassistant.components.sensor import SensorEntity
 from homeassistant.const import DEVICE_CLASS_ENERGY
 from homeassistant.const import ENERGY_KILO_WATT_HOUR
 from homeassistant.helpers.entity import Entity, generate_entity_id
 from homeassistant.components.sensor import ENTITY_ID_FORMAT
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from . import DOMAIN, CONF_METER_ID
 
@@ -12,7 +19,12 @@ import logging
 _LOGGER = logging.getLogger(__name__)
 
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None
+) -> None:
     """Set up the sensor platform."""
     # We only want this platform to be set up via discovery.
     if discovery_info is None:
@@ -36,14 +48,19 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
 
     add_entities([Mojelektro("15min_output", hass, meter_id)])
     add_entities([Mojelektro("15min_input", hass, meter_id)])
+    
+    add_entities([Mojelektro("monthly_input", hass, meter_id)])
+    add_entities([Mojelektro("monthly_input_peak", hass, meter_id)])
+    add_entities([Mojelektro("monthly_input_offpeak", hass, meter_id)])
 
 
-class Mojelektro(Entity):
+
+class Mojelektro(SensorEntity):
     """Representation of a sensor."""
 
     type = None
 
-    def __init__(self, type, hass, meter_id):
+    def __init__(self, type, hass, meter_id) -> None:
         """Initialize the sensor."""
         super().__init__()
 
@@ -58,7 +75,7 @@ class Mojelektro(Entity):
         return self._unique_id
     
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the name of the sensor."""
         return "MojElektro " + self.type
 
@@ -68,16 +85,21 @@ class Mojelektro(Entity):
         return self._state
 
     @property
-    def unit_of_measurement(self):
+    def unit_of_measurement(self) -> str:
         """Return the unit of measurement."""
         return ENERGY_KILO_WATT_HOUR
+
+    @property
+    def state_class(self):
+        """Return the state class."""
+        return "total_increasing"
 
     @property
     def device_class(self):
         """Return the device class."""
         return DEVICE_CLASS_ENERGY
     
-    def update(self):
+    def update(self) -> None:
         """Fetch new state data for the sensor.
 
         This is the only method that should fetch new data for Home Assistant.
