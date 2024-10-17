457877ee09adf0e60399f496dd91551b6f0f62dd
Simon <simon.medved@alexsolutions.com.au>
2024-10-17 20:37:01 +0200

    Fix deprecated messages

--- custom_components/mojelektro/__init__.py
+++ custom_components/mojelektro/__init__.py
@@ -15,7 +15,7 @@ async def async_setup(hass: HomeAssistant, config: dict):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, "sensor"))
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
